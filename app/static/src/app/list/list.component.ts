53b5381d8b66ada87ac2c571d5b2d65dff3b6b52
LucaVazz <luca.vazzano@gmx.net>
2017-12-15 22:29:30 +0100

    add component routing (cough cough)

--- app/static/src/app/list/list.component.ts
+++ app/static/src/app/list/list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Data } from './../data';
+
 
 @Component({
   selector: 'app-list',
@@ -6,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+
+    @Input() data: Data;
 
   constructor() { }
 
