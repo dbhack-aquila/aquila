c13f7fd0a176182e5028959def21bc49d104172c
Momo_Hunter <moris_92@web.de>
2017-12-16 01:17:41 +0100

    added details site for pois

--- app/static/src/app/app.component.ts
+++ app/static/src/app/app.component.ts
@@ -24,6 +24,6 @@ export class AppComponent {
 
   ngOnInit() {
       this.data = this.dataService.get("ICE1337", 17);
-      this.view = 'start';
+      this.view = 'details';
   }
 }
--- app/static/src/app/details/details.component.ts
+++ app/static/src/app/details/details.component.ts
@@ -14,7 +14,44 @@ export class DetailsComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+    directPictureLinksLine1 = []
+	directPictureLinksLine2 = []
+	directPictureLinksLine3 = []
+	modalActive = false
+	imageLink = ""
+	imageName = ""
+
+	ngOnInit() {
+		let amount = this.poi.imageUrls.length
+		let rest = amount % 3
+		let additional = 0
+		if(rest < 3 && rest > 0){
+			additional = 1
+		}
+		for(var i = 0; i < ((amount - rest) / 3) + additional; i++){
+			this.directPictureLinksLine1.push({url: this.poi.imageUrls[i]})
+		}
+		if(rest == 2){
+			additional = 2
+		}
+		else {
+			additional = 0
+		}
+		for(i; i < (((amount - rest) / 3) * 2) + additional; i++){
+			this.directPictureLinksLine2.push({url: this.poi.imageUrls[i]})
+		}
+		for(i; i < amount; i++){
+			this.directPictureLinksLine3.push({url: this.poi.imageUrls[i]})
+		}
+	}
+
+	showModal(link){
+		this.imageLink = link.url
+		this.modalActive = true
+	}
+
+	closeModal(){
+		this.modalActive = false
+	}
 
 }
