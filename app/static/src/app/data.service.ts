ce0448e6b0902f46edafd72b8797646271e3e9b7
Momo_Hunter <moris_92@web.de>
2017-12-16 06:59:55 +0100

    get integrated

--- app/static/src/app/data.service.ts
+++ app/static/src/app/data.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs/Observable';
+import "rxjs/add/observable/of";
 
 import { Data } from './data';
 import { Poi } from './poi';
@@ -7,24 +11,9 @@ import { Poi } from './poi';
 @Injectable()
 export class DataService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  get(no: number) {
-      return new Data(
-          52.514436,
-          13.418947,
-          [
-              new Poi("Mindbox", "Wir sind Teil der Digitalisierungsaktivitäten der DB unter DB digital. Die DB mindbox ist ein zentraler Ort, an dem wir uns mit Startups austauschen und zusammenarbeiten.", "https://inside.bahn.de/wordpress/uploads/2016/02/Mindbox2-Kopie-800x534.jpg", 52.514136, 13.419848, ["http://dbmindbox.com/dbaccelerator/", "https://www.facebook.com/dbmindbox/"], ["https://inside.bahn.de/wordpress/uploads/2016/02/Mindbox2-Kopie-800x534.jpg"]),
-              new Poi("Jannowitzbrücke1", "Coole Brücke, much Fluss, very bogig", "https://s3.amazonaws.com/gs-geo-images/6db8dbfb-2bc6-42f0-a1ea-25acb8a2ded8_l.jpg", 52.514240, 13.417788, ["https://de.wikipedia.org/wiki/Jannowitzbr%C3%BCcke", "https://www.berlin.de/tourismus/dampferfahrten/anlegestellen/2488866-2486252-jannowitzbruecke.html", "http://www.s-bahn-berlin.de/fahrplanundnetz/bahnhof/jannowitzbruecke/84"], [""]),
-              new Poi("Jannowitzbrücke2", "Coole Brücke, much Fluss, very bogig", "https://s3.amazonaws.com/gs-geo-images/6db8dbfb-2bc6-42f0-a1ea-25acb8a2ded8_l.jpg", 52.514240, 13.417788, ["https://de.wikipedia.org/wiki/Jannowitzbr%C3%BCcke", "https://www.berlin.de/tourismus/dampferfahrten/anlegestellen/2488866-2486252-jannowitzbruecke.html", "http://www.s-bahn-berlin.de/fahrplanundnetz/bahnhof/jannowitzbruecke/84"], [""]),
-              new Poi("Jannowitzbrücke3", "Coole Brücke, much Fluss, very bogig", "https://s3.amazonaws.com/gs-geo-images/6db8dbfb-2bc6-42f0-a1ea-25acb8a2ded8_l.jpg", 52.514240, 13.417788, ["https://de.wikipedia.org/wiki/Jannowitzbr%C3%BCcke", "https://www.berlin.de/tourismus/dampferfahrten/anlegestellen/2488866-2486252-jannowitzbruecke.html", "http://www.s-bahn-berlin.de/fahrplanundnetz/bahnhof/jannowitzbruecke/84"], [""]),
-              new Poi("kek", "Coole Brücke, much Fluss, very bogig", "https://s3.amazonaws.com/gs-geo-images/6db8dbfb-2bc6-42f0-a1ea-25acb8a2ded8_l.jpg", 52.514240, 13.417788, ["https://de.wikipedia.org/wiki/Jannowitzbr%C3%BCcke", "https://www.berlin.de/tourismus/dampferfahrten/anlegestellen/2488866-2486252-jannowitzbruecke.html", "http://www.s-bahn-berlin.de/fahrplanundnetz/bahnhof/jannowitzbruecke/84"], [""]),
-              new Poi("Jannowitzbrücke4", "Coole Brücke, much Fluss, very bogig", "https://s3.amazonaws.com/gs-geo-images/6db8dbfb-2bc6-42f0-a1ea-25acb8a2ded8_l.jpg", 52.514240, 13.417788, ["https://de.wikipedia.org/wiki/Jannowitzbr%C3%BCcke", "https://www.berlin.de/tourismus/dampferfahrten/anlegestellen/2488866-2486252-jannowitzbruecke.html", "http://www.s-bahn-berlin.de/fahrplanundnetz/bahnhof/jannowitzbruecke/84"], [""]),
-              new Poi("Jannowitzbrücke5", "Coole Brücke, much Fluss, very bogig", "https://s3.amazonaws.com/gs-geo-images/6db8dbfb-2bc6-42f0-a1ea-25acb8a2ded8_l.jpg", 52.514240, 13.417788, ["https://de.wikipedia.org/wiki/Jannowitzbr%C3%BCcke", "https://www.berlin.de/tourismus/dampferfahrten/anlegestellen/2488866-2486252-jannowitzbruecke.html", "http://www.s-bahn-berlin.de/fahrplanundnetz/bahnhof/jannowitzbruecke/84"], [""]),
-              new Poi("Jannowitzbrücke6", "Coole Brücke, much Fluss, very bogig", "https://s3.amazonaws.com/gs-geo-images/6db8dbfb-2bc6-42f0-a1ea-25acb8a2ded8_l.jpg", 52.514240, 13.417788, ["https://de.wikipedia.org/wiki/Jannowitzbr%C3%BCcke", "https://www.berlin.de/tourismus/dampferfahrten/anlegestellen/2488866-2486252-jannowitzbruecke.html", "http://www.s-bahn-berlin.de/fahrplanundnetz/bahnhof/jannowitzbruecke/84"], [""]),
-              new Poi("Jannowitzbrücke7", "Coole Brücke, much Fluss, very bogig", "https://s3.amazonaws.com/gs-geo-images/6db8dbfb-2bc6-42f0-a1ea-25acb8a2ded8_l.jpg", 52.514240, 13.417788, ["https://de.wikipedia.org/wiki/Jannowitzbr%C3%BCcke", "https://www.berlin.de/tourismus/dampferfahrten/anlegestellen/2488866-2486252-jannowitzbruecke.html", "http://www.s-bahn-berlin.de/fahrplanundnetz/bahnhof/jannowitzbruecke/84"], [""])
-          ]
-      )
-  }
-
+  get(no: number): Observable<Data> {
+        return this.http.get<Data>('http://127.0.0.1:5000/gps/40117905/' + no);
+    }
 }
